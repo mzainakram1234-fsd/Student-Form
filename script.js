369534cac8e07627b664253889e790b1195f290e
mzainakram1234-fsd <m.akramzain1234@gmail.com>
2025-10-08 20:59:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myfunction(){
+    window.alert("YOUR FORM SUBMITTED SUCCESSFULLY.....THANK YOU FOR YOUR REGARDS ")
+}
+function myfun(){
+    window.alert("YOU FILL ALL REQUIRMENTS OF THE FIRST PART OF THE FORM......THANKS FOR YOUR REGARDS ")
+}
